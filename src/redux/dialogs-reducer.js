a58ae53afed9194454f098fdb28b8d4f0a944bdd
Тарас Іванович <vip.kristals@gmail.com>
2023-01-26 21:40:54 +0200

    11-redux

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -1,7 +1,25 @@
 const ADD_MESSAGE = "ADD-MESSAGE";
 const UPDATE_NEW_MESSAGE = "UPDATE-NEW-MESSAGE";
 
-const dialogsReducer = (state,action) =>{
+let initialState = {
+    messages: [
+        {id: 1, message: 'Hello'},
+        {id: 2, message: 'How are you?'},
+        {id: 3, message: 'Yo'},
+        {id: 4, message: 'Yo'},
+        {id: 5, message: 'Ya'},
+    ],
+    dialogs: [
+        {id: 1, name: 'Taras'},
+        {id: 2, name: 'Vasyl'},
+        {id: 3, name: 'Viktor'},
+        {id: 4, name: 'Vika'},
+        {id: 5, name: 'Valera'},
+        {id: 6, name: 'Dima'},
+    ],
+    newMessage:"Hello"
+}
+const dialogsReducer = (state = initialState,action) =>{
     switch (action.type) {
         case ADD_MESSAGE:
             let addMessage = {
