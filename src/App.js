e2d997906965b0e4018a01e4e9ee8bf67505b5f2
TarasGogol <72978940+TarasGogol@users.noreply.github.com>
2023-02-03 21:57:50 +0200

    22- Presentational component, container component

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Settings from "./components/Settings/Settings";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 
+
 function App(props) {
   return (
       <BrowserRouter>
---
+++ src/components/Users/Users.js
@@ -0,0 +1,52 @@
+import React from "react";
+import styles from "./user.module.css";
+import photoUser from "../../assets/avatar.png";
+
+let Users = (props) => {
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize)
+
+    let pages = [];
+
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
+    }
+    let curP = props.currentPage;
+    let curPF = ((curP - 5) < 0) ? 0 : curP - 5;
+    let curPL = curP + 5;
+    let slicedPages = pages.slice(curPF, curPL)
+
+    return <div>
+        <div>
+            {slicedPages.map(p => {
+                return <span className={props.currentPage === p && styles.selectedUser}
+                             onClick={(e) => {
+                                 props.onPageChanged(p)
+                             }}>{p}</span>
+            })}
+        </div>
+        {
+            props.users.map(u => <div key={u.id}>
+                <span>
+                    <div>
+                        <img src={u.photos.small != null ? u.photos.small : photoUser} className={styles.photoUser}/>
+                    </div>
+                     <div>
+                    {u.followed
+                        ? <button onClick={() => {
+                            props.unfollow(u.id)
+                        }}>Unfollow</button>
+                        : <button onClick={() => {
+                            props.follow(u.id)
+                        }}>Follow</button>}
+                    </div>
+                </span>
+                <span>
+                    <div>{u.name}</div>
+                    <div>{"u.email"}</div>
+                    <div>{"u.location.city"},{"u.location.country"}</div>
+                </span>
+            </div>)
+        }
+    </div>
+}
+export default Users;
