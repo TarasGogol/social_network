9e40098abc04f50180c050cdbbcae12a43e766f3
TarasGogol <72978940+TarasGogol@users.noreply.github.com>
2023-02-09 13:46:19 +0200

    27 - cookie,login

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import style from './Header.module.css'
+import {NavLink} from "react-router-dom";
 
 
-const Header = () => {
+const Header = (props) => {
     return (
         <header className={style.header}>
             <img src="https://media.istockphoto.com/id/1321507706/vector/letter-vf-outline-business-logo-design.jpg?s=612x612&w=0&k=20&c=lw4zjZuc7F6bYAeiruQ_IwhODElqnUt9g3jM7UUczsQ=" alt="description of image"/>
+
+            <div className={style.loginBlock}>
+                {props.isAuth ? props.login
+                    :<NavLink to={"/login"}>Login</NavLink>}
+            </div>
         </header>
     );
 };
