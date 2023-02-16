843a4a634dc8a7d89d0ea9e98101774adc3cbd44
TarasGogol <72978940+TarasGogol@users.noreply.github.com>
2023-02-16 12:47:26 +0200

    34 - localState, setState

--- src/components/Profile/ProfileContainer.js
+++ src/components/Profile/ProfileContainer.js
@@ -2,7 +2,9 @@ import React from 'react';
 import {getUserProfile} from "../../redux/profile-reducer";
 import {connect} from "react-redux";
 import Profile from "./Profile";
-import {Navigate, useParams} from "react-router-dom";
+import {useParams} from "react-router-dom";
+import {withAuthRedirect} from "../hoc/withAuthRedirect";
+import {compose} from "redux";
 
 export function withRouter(Children){
     return(props)=>{
@@ -22,7 +24,7 @@ class ProfileContainer extends React.Component{
     }
 
     render() {
-        if(!this.props.isAuth) return <Navigate to={"/login"}/>
+
         return (
             <Profile {...this.props}
                      profile={this.props.profile}/>
@@ -30,10 +32,11 @@ class ProfileContainer extends React.Component{
     }
 }
 let mapStateToProps = (state) => ({
-    profile:state.profilePage.profile,
-    isAuth: state.auth.isAuth
+    profile:state.profilePage.profile
 })
 
-let withUrlDataContainerComponent = withRouter(ProfileContainer)
-
-export default connect(mapStateToProps,{getUserProfile})(withUrlDataContainerComponent);
+export default compose(
+    connect(mapStateToProps,{getUserProfile}),
+    withRouter,
+    withAuthRedirect,
+)(ProfileContainer)
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import style from '../Profile.module.css'
 import Preloader from "../../common/Preloader/Preloader";
+import ProfileStatus from "./ProfileStatus"
 
 
 const ProfileInfo = (props) => {
@@ -14,7 +15,10 @@ const ProfileInfo = (props) => {
                 <img src="https://images.pexels.com/photos/9754/mountains-clouds-forest-fog.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="img forest"/>
             </div>
             <div className={style.ava}>
-                <img src={props.profile.data.photos.large}/>{props.profile.data.aboutMe}</div>
+                <img src={props.profile.data.photos.large}/>
+                <ProfileStatus status={"Hello ded"}/>
+            </div>
+            <h2>My posts</h2>
         </div>
     );
 };
